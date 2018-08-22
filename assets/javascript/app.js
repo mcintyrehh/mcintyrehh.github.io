b6c16db3565b96aa3cec57582e983784a0543708
Onray <mcintyrehh@gmail.com>
2018-08-22 16:27:54 -0400

    added contact section, working submit function coming soon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,48 +15,68 @@ $(document).ready(function () {
     );
 
 });
-
-var contactArray = ["let contact = {\n", "intro: \"I\'d love to hear from you\",\n", "strangers: \"say hi\",\n", "friends: \"hurl an insult\",\n", "recruiters: \"send job offers!\"\n"]
+//array with an element for each line printd
+var contactArray = ["let contact = {", "intro: \"I\'d love to hear from you\",", "strangers: \"say hi\",", "friends: \"hurl an insult\",", "recruiters: \"send job offers!\"", "}"]
 console.log(contactArray);
+var caption = $("#caption");
+//declaring iterators for index of individual elements, and the elements themselves
 var i = 0;
 var index = 0;
 var currentWord = contactArray[index];
-var text = "Testing testing testing";
-var speed = 500; /* The speed/duration of the effect in milliseconds */
-
+//as long as i is within the word, append the letter at this place and move to the next letter
 function typeWriter() {
-    // console.log(i);
-    console.log(currentWord);
-    
     if (i < currentWord.length) {
-        $("#caption").append(currentWord.charAt(i))
+        caption.append(currentWord.charAt(i))
         i++;
     }
+    //once the whole line has been printed, reset i, move to next element
     else {
         i = 0;
         index++
-        
         currentWord = contactArray[index];
-        console.log(index);
-        clearInterval(interval)
-        setInterval(typeWriter, 500);
-        if (index === contactArray.length) {
+        if (index === 5) {
+            caption.append("<br />} <span class=\"blink\">▓<span>");
+            setInterval(blink, 400);
+            // console.log(currentWord);
+            stopFunction();
             return;
+        }
+        else {
+            caption.append("<br /> &nbsp &nbsp &nbsp &nbsp");
+            console.log(currentWord);
+        }
+        //exit and stop the interval once all elements have been displayed
+        if (index === contactArray.length) {
             console.log("breaking here");
-            clearInterval(interval);
+            stopFunction();
+            return;
         }
 
     }
 }
-var interval = setInterval(typeWriter, 100)
+//appends a letter ia typeWriter every 150ms
+var interval = setInterval(typeWriter, 150)
 //callback function to clear the interval once line is completed
 function stopFunction() {
     clearInterval(interval);
 }
-// for (var i = 0; i < contactArray.length; i++) {
-//     text = contactArray[i];
-//     console.log(text);
-//     typeWriter();
-// }
-
-// // setInterval()
+//ternary operator would be better here, but if opacity is 0, makes it 1 and if 1, makes it 0
+//blinking animation for the cursor
+function blink() {
+    var pipe = $(".blink");
+    if (pipe.css("opacity") == 1) {
+        pipe.css("opacity", 0)
+    }
+    else {
+        pipe.css("opacity", 1);
+    }
+}
+//grab the user inputs on clicking submit
+$(".submit").on("click", function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    console.log("Name: " + name);
+    console.log("email: " + email);
+    console.log("message: " + message);
+})
